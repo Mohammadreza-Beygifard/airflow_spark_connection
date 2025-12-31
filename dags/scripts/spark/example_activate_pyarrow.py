70811d42411cc0db70aaff2713ffeac60cb94129
Mohammadreza Beygifard <samanbeygifard73@gmail.com>
2025-12-31 11:04:45 +0100

    Add PyArrow and PyArrow example DAG

---
+++ dags/scripts/spark/example_activate_pyarrow.py
@@ -0,0 +1,45 @@
+import time
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, sin
+
+# Create Spark session
+spark: SparkSession = SparkSession.getActiveSession()
+if not spark:
+    spark = SparkSession.builder.appName("Spark Submit job With PyArrow").getOrCreate()
+
+N = 5_000_000  # adjust depending on machine
+
+df = (
+    spark.range(N).withColumn("x", col("id") * 2).withColumn("y", sin(col("id") * 0.01))
+)
+
+# Trigger Spark execution once (warm-up)
+df.show(5)
+print("Row count:", df.count())
+
+# -------------------------------
+# WITHOUT Arrow
+# -------------------------------
+spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "false")
+
+start = time.perf_counter()
+pdf_without_arrow = df.toPandas()
+end = time.perf_counter()
+
+print(f"\n ****** toPandas WITHOUT Arrow: {end - start:.2f} seconds ******")
+
+# -------------------------------
+# WITH Arrow
+# -------------------------------
+spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
+spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", "10000")
+spark.conf.set("spark.sql.execution.arrow.pyspark.fallback.enabled", "true")
+
+start = time.perf_counter()
+pdf_with_arrow = df.toPandas()
+end = time.perf_counter()
+
+print(f"\n ****** toPandas WITH Arrow: {end - start:.2f} seconds ******")
+
+# Cleanup
+spark.stop()
---
+++ dags/spark_pyarrow_submit_dag.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+
+with DAG(
+    dag_id="spark_pyarrow_submit_dag",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval=None,  # Run on demand
+    catchup=False,
+    tags=["practice", "branching"],
+) as dag:
+
+    start = EmptyOperator(task_id="start")
+    spark_task = SparkSubmitOperator(
+        task_id="spark_task",
+        application="/opt/airflow/dags/scripts/spark/example_activate_pyarrow.py",  # This is mounted inside containers
+        conn_id="spark_conn",  # gets connected to spark://spark-master:7077
+        verbose=True,
+    )
+
+    start >> spark_task
